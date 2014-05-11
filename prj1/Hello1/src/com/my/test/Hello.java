8c59b7198f49010d0bfc5f5780ed27c97e601218
tbn <tbn@xebas.de>
2014-05-11 03:22:16 +0200

    commit1

---
+++ prj1/Hello1/src/com/my/test/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.my.test;
+
+/**
+ * @author tbn
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
